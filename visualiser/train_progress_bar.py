64e7022a077608da9d3282f4510c2a4eba14427c
Jiayou Song <--replace-all>
2022-08-27 13:17:40 +0800

    basic visualiser implemented (add files)

---
+++ visualiser/train_progress_bar.py
@@ -0,0 +1,15 @@
+import ipywidgets as widgets
+
+class TrainProgressBar:
+    def __init__(self):
+        self.s = widgets.IntProgress(value=0,min=0,max=365*24)
+
+    def initialize_progress(self,episode):
+        self.s.max = 365*24*episode
+        self.s.value = 0
+
+    def update_progress_by_one(self):
+        self.s.value += 1
+
+    def get_object(self):
+        return self.s

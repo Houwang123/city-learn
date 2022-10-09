21d2a3daf489ee36a5334e6754aaf0a5a7f96112
Jiayou Song <--replace-all>
2022-10-09 10:03:42 +0100

    rbc 2.66

--- agents/agents/rbc.py
+++ agents/agents/rbc.py
@@ -11,34 +11,31 @@ def daytime_phase(month, day, hour):
     else:
         return 0
 
-def separate_rbc_policy(observation, agent_id):
-    month = observation[0]
-    day = observation[1]
-    hour = observation[2]
-    daytime_phase_value = daytime_phase(month, day, hour)
-    
-    action = 0.0 # Default value
-    if daytime_phase_value == 1:
-        if agent_id == 4:
-            action = -0.04
-        else:
-            action = 0.24
-    elif daytime_phase_value == 0:
-        action = -0.1
-
-    return action
-
-def rbc_policy(observation):
-    """
-    Simple rule based policy based on day or night time
-    """
-    actions = np.array([[separate_rbc_policy(observation[id], id)] for id in range(5)], dtype=np.float64)
-    return actions
-
-
 class RBCAgent():
-    
-    def __init__(self, ):
+    def separate_rbc_policy(self, observation, agent_id):
+        month = observation[0]
+        day = observation[1]
+        hour = observation[2]
+        daytime_phase_value = daytime_phase(month, day, hour)
+        
+        action = 0.0 # Default value
+        if daytime_phase_value == 1:
+            if agent_id == 4:
+                action = -0.04 + 0.00193223620844446
+            else:
+                action = 0.24 + -0.005077212243585316
+        elif daytime_phase_value == 0:
+            action = -0.1 + -0.015223804448657952
+        return action
+
+    def rbc_policy(self, observation):
+        """
+        Simple rule based policy based on day or night time
+        """
+        actions = np.array([[self.separate_rbc_policy(observation[id], id)] for id in range(5)], dtype=np.float64)
+        return actions
+
+    def __init__(self):
         pass
 
     def register_reset(self, observation, training = True):
@@ -46,7 +43,7 @@ class RBCAgent():
 
     def compute_action(self, observation):
         """Get observation return action"""
-        return rbc_policy(observation)
+        return self.rbc_policy(observation)
 
     def update(self, s, a, r, ns): 
         pass
